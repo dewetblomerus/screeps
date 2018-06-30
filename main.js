d6f7f28cd162d5f4dc3808c31d8d8bcefb17863d
De Wet Blomerus <dewet@blomerus.org>
2018-06-30 17:32:54 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,40 @@
+var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
+
+module.exports.loop = function() {
+  var tower = Game.getObjectById('id772680');
+  if (tower) {
+    var closestDamagedStructure = tower.pos.findClosestByRange(
+      FIND_STRUCTURES,
+      {
+        filter: structure => structure.hits < structure.hitsMax
+      }
+    );
+    if (closestDamagedStructure) {
+      tower.repair(closestDamagedStructure);
+    }
+
+    var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+    if (closestHostile) {
+      tower.attack(closestHostile);
+    }
+  }
+
+  Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], 'Worker1', {
+    memory: { role: 'harvester' }
+  });
+
+  for (var name in Game.creeps) {
+    var creep = Game.creeps[name];
+    if (creep.memory.role == 'harvester') {
+      roleHarvester.run(creep);
+    }
+    if (creep.memory.role == 'upgrader') {
+      roleUpgrader.run(creep);
+    }
+    if (creep.memory.role == 'builder') {
+      roleBuilder.run(creep);
+    }
+  }
+};
