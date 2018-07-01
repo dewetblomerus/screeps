a05aef24a3ebbc32cbe71c34e7fb3d9501911b03
De Wet Blomerus <dewet@blomerus.org>
2018-07-01 09:06:53 +0200

    creepCounter

--- creep.spawner.js
+++ creep.spawner.js
@@ -1,9 +1,15 @@
+var creepCounter = {
+  run: function() {
+    console.log(Object.keys(Game.creeps).length);
+    return Object.keys(Game.creeps).length;
+  }
+};
+
 var creepSpawner = {
   run: function() {
-    console.log('from screepspawner');
-    if (Object.keys(Game.creeps).length < 2) {
+    if (creepCounter.run() < 2) {
       console.log('we have less than two creeps');
-      Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], 'Worker1', {
+      Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], {
         memory: { role: 'harvester' }
       });
     } else {
