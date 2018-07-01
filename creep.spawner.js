103bbe2fc70246b98f2a91ab62551e1aa13698a4
De Wet Blomerus <dewet@blomerus.org>
2018-07-01 14:20:44 +0200

    roleName

--- creep.spawner.js
+++ creep.spawner.js
@@ -12,16 +12,17 @@ var creepCounter = {
 };
 
 var creepSpawner = {
-  targetNumber: 4,
+  targetNumber: 7,
+  creepRole: 'harvester',
   run: function() {
-    console.log(`Harvester Count: ${creepCounter.run('harvester')}`);
-    if (creepCounter.run('harvester') < this.targetNumber) {
+    console.log(`Upgrader Count: ${creepCounter.run(this.creepRole)}`);
+    if (creepCounter.run(this.creepRole) < this.targetNumber) {
       console.log(`we have less than ${this.targetNumber} creeps`);
       const result = Game.spawns['Spawn1'].spawnCreep(
         [WORK, CARRY, MOVE],
         `Creep ${Math.floor(Math.random() * 1000 + 1)}`,
         {
-          memory: { role: 'harvester' }
+          memory: { role: this.creepRole }
         }
       );
       if (_.isString(result)) {
@@ -30,7 +31,7 @@ var creepSpawner = {
         console.log('Spawn error: ' + result);
       }
     } else {
-      console.log('we have more than one creep');
+      console.log(`We have enough creeps`);
     }
   }
 };
