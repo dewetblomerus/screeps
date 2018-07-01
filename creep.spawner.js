df1d315723564bae2405cf9592bc13647689e1c6
De Wet Blomerus <dewet@blomerus.org>
2018-07-01 08:27:26 +0200

    auto-create one screep with hardcoded name

--- creep.spawner.js
+++ creep.spawner.js
@@ -1,8 +1,14 @@
 var creepSpawner = {
   run: function() {
-    Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], 'Worker1', {
-      memory: { role: 'harvester' }
-    });
+    console.log('from screepspawner');
+    if (Object.keys(Game.creeps).length < 2) {
+      console.log('we have less than two creeps');
+      Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], 'Worker1', {
+        memory: { role: 'harvester' }
+      });
+    } else {
+      console.log('we have more than one screep');
+    }
   }
 };
 
---
+++ reference.js
@@ -0,0 +1,11 @@
+Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], 'Harvester1');
+Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], 'Harvester2');
+
+if (Object.keys(Game.creeps).length < 2) {
+  console.log('we have less than two creeps');
+  Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], 'Worker1', {
+    memory: { role: 'harvester' }
+  });
+} else {
+  console.log('we have more than one screep');
+}
