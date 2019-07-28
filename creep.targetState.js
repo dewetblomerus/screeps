18af1b625aa7e41588a0badd74c1874177dbbf98
De Wet Blomerus <dewet@blomerus.org>
2019-07-28 10:44:26 +0200

    harvester still alive

--- creep.targetState.js
+++ creep.targetState.js
@@ -135,9 +135,10 @@ const targetState = room => {
     }
   }
 
+  // console.log('default target state')
   return {
     worker: { amount: 2, priority: 0 },
-    carrier: { amount: 1, priority: 0 },
+    carrier: { amount: 1, priority: 1 },
     upgrader: { amount: 3, priority: 2 },
   }
 }
